5a49879478dfe56f3b6e1917f4aca49b71e89909
Ieva-source <petrauskaite.ieva14@gmail.com>
2020-02-19 14:27:19 +0100

    One-way ANOVA'S

--- One-way ANOVA.R
+++ One-way ANOVA.R
@@ -1,20 +1,25 @@
+#########################################################################
 ###Testing for a treament (grazing) effect in permanent plots############
 ########One-Way ANOVA####################################################
 
-Testing<-read.csv("Testing_for_treatment_effect.csv",  
+A<-read.csv("Total_Biomass_Permanent_Plots.csv",  
                     sep=";",                        
                     dec=",",                        
                     header=TRUE,                    
                     stringsAsFactors = FALSE)      
 
-attach(Testing)
-is.factor(Treatment)
+attach(A)
+is.factor(A)
+
+################################
+###Looking into TOTAL BIOMASS###
+################################
 
 
-boxplot(Biomass~Treatment, data=Testing, main="Biomass in caged and open permanent plots", xlab="Treatment", ylab="Total above-ground biomass (g)",
+boxplot(Total.Biomass~Treatment, data=A, main="Biomass in caged and open permanent plots", xlab="Treatment", ylab="Total above-ground biomass (g)",
         col=rainbow(4))
 
-model1<-aov(Biomass~Treatment)
+model1<-aov(Total.Biomass~Treatment)
 summary (model1)
 
 par(mfrow=c(2,2))
@@ -27,4 +32,59 @@ shapiro.test(uhat)
 
 #####Testing for homogeneity (bartlett test)####
 
-bartlett.test(Biomass~Treatment)
+bartlett.test(Total.Biomass~Treatment)
+
+#################################################
+###########Looking into BIO/DAY data#############
+#################################################
+
+par(mfrow=c(1,1))
+
+boxplot(bioperday~Treatment, data=A, main="Bio/day in caged and open permanent plots", xlab="Treatment", ylab="Biomass/day (g)",
+        col=rainbow(4))
+
+model2<-aov(bioperday~Treatment)
+summary (model2)
+
+par(mfrow=c(2,2))
+plot(model2)
+
+#####Shapiro-Wilk test (testing for normality)####
+
+uhat2<-resid(model2)
+shapiro.test(uhat2)
+
+#####Testing for homogeneity (bartlett test)####
+
+bartlett.test(bioperday~Treatment)
+
+##################################################
+###########Looking into BIO/DAY data #############
+#####when the in between has + 32 days added######
+
+A<-read.csv("Total_Biomass_Permanent_Plots.csv",  
+            sep=";",                        
+            dec=",",                        
+            header=TRUE,                    
+            stringsAsFactors = FALSE)  
+attach(A)
+
+par(mfrow=c(1,1))
+
+boxplot(bioperday32~Treatment, data=A, main="Bio/day in caged and open permanent plots", xlab="Treatment", ylab="Biomass/day (g)",
+        col=rainbow(4))
+
+model3<-aov(bioperday32~Treatment)
+summary (model3)
+
+par(mfrow=c(2,2))
+plot(model3)
+
+#####Shapiro-Wilk test (testing for normality)####
+
+uhat3<-resid(model3)
+shapiro.test(uhat3)
+
+#####Testing for homogeneity (bartlett test)####
+
+bartlett.test(bioperday32~Treatment)
