8bdcdcec7e56cb4aab4db8a6fcb39221ed98b37b
Ieva-source <petrauskaite.ieva14@gmail.com>
2020-02-20 14:16:27 +0100

    Ggplots some updates

--- ggplot.R
+++ ggplot.R
@@ -1,3 +1,4 @@
+########################################################################
 ###Trying the ggplot to nicely visualize the biomass data from permanent plots###
 #################################################################################
 
@@ -176,4 +177,65 @@ p10 <- ggplot(Permanent, aes(x = elevation, y = bioperday32, fill = Treatment)) 
   labs(fill = "Treatment")
 p10
 
+################################################################
+####Let's look into the functional groups in permanent plots####
+################################################################
 
+F<-read.csv("Functional_Group_Biomass_Permanent_Plots.csv",  
+                    sep=";",                        
+                    dec=",",                        
+                    header=TRUE,                    
+                    stringsAsFactors = FALSE)
+
+library(ggplot2)
+
+data(F)
+F$Site <- factor(F$Site,
+                              labels = c("Vikesland", "Hogsete", "Joasete", "In Between", "Liahovden"))
+
+p10 <- ggplot(F, aes(x = Site, y = Gperday, fill = Treatment)) +
+  geom_boxplot()
+p10
+
+p10 <- ggplot(F, aes(x = Site, y = Gperday, fill = Treatment)) +
+  geom_boxplot(notch = FALSE) + scale_x_discrete(name = "Study sites") +
+  scale_y_continuous(name = "Biomass/day (g)") + ggtitle("Graminoid above-ground biomass grown per day in permanent plots across sites") + scale_fill_brewer(palette = "Accent") + theme_bw() +
+  theme(plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
+        text = element_text(size = 12, family = "Tahoma"),
+        axis.title = element_text(face="bold"),
+        axis.text.x=element_text(size = 11),
+        legend.position = "bottom") +
+  scale_fill_brewer(palette = "Accent") +
+  labs(fill = "Treatment")
+p10
+
+pF <- ggplot(F, aes(x = Site, y = Fperday, fill = Treatment)) +
+  geom_boxplot()
+pF
+
+pF <- ggplot(F, aes(x = Site, y = Fperday, fill = Treatment)) +
+  geom_boxplot(notch = FALSE) + scale_x_discrete(name = "Study sites") +
+  scale_y_continuous(name = "Biomass/day (g)") + ggtitle("Forbs above-ground biomass grown per day in permanent plots across sites") + scale_fill_brewer(palette = "Accent") + theme_bw() +
+  theme(plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
+        text = element_text(size = 12, family = "Tahoma"),
+        axis.title = element_text(face="bold"),
+        axis.text.x=element_text(size = 11),
+        legend.position = "bottom") +
+  scale_fill_brewer(palette = "Accent") +
+  labs(fill = "Treatment")
+pF
+
+######################################################################
+####Attempt to put multiple plots together############################
+######################################################################
+
+install.packages("ggpubr")
+library(ggpubr)
+
+ggarrange(p10, pF + rremove("x.text"), 
+          labels = c("Graminoids", "Forbs"),
+          ncol = 1, nrow = 2)
+
+install.packages("installr"); library(installr) # install+load installr
+
+updateR()
