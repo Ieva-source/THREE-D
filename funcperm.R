8556a76b169f19200386cb42a828a0c627392fd8
Ieva-source <petrauskaite.ieva14@gmail.com>
2020-02-24 14:48:07 +0100

    Func_Biomass_Permanent

--- funcperm.R
+++ funcperm.R
@@ -9,36 +9,46 @@ F<-read.csv("Functional_Group_Biomass_Permanent_Plots.csv",
             stringsAsFactors = FALSE)
 
 library(ggplot2)
+library(GGally)
+library(dplyr)
+attach(F)
 
-data(F)
+attach(F)
 F$Site <- factor(F$Site,
-                 labels = c("V", "H", "J", "B", "L"))
+                 labels = c("469", "700", "920", "1100", "1290"))
 
-p10 <- ggplot(F, aes(x = Site, y = Gperday, fill = Treatment)) +
+#############################################################################
+#############################################################################
+##Graminoids##
+
+pG <- ggplot(F, aes(x = Site, y = Gperday, fill = Treatment)) +
   geom_boxplot()
-p10
+pG
 
-p10 <- ggplot(F, aes(x = Site, y = Gperday, fill = Treatment)) +
-  geom_boxplot(notch = FALSE) + scale_x_discrete(name = "Study sites") +
+pG <- ggplot(F, aes(x = Site, y = Gperday, fill = Treatment)) +
+  geom_boxplot(notch = FALSE) + scale_x_discrete(name = "Elevation") +
   scale_y_continuous(name = "Biomass/day (g)") + ggtitle("Graminoids") + scale_fill_brewer(palette = "Accent") + theme_bw() +
   theme(plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
-        text = element_text(size = 12, family = "Tahoma"),
+        text = element_text(size = 10, family = "Tahoma"),
         axis.title = element_text(face="bold"),
         axis.text.x=element_text(size = 11),
         legend.position = "bottom") +
   scale_fill_brewer(palette = "Accent") +
   labs(fill = "Treatment")
-p10
+pG
+
+
+##Forbs##
 
 pF <- ggplot(F, aes(x = Site, y = Fperday, fill = Treatment)) +
   geom_boxplot()
 pF
 
 pF <- ggplot(F, aes(x = Site, y = Fperday, fill = Treatment)) +
-  geom_boxplot(notch = FALSE) + scale_x_discrete(name = "Study sites") +
+  geom_boxplot(notch = FALSE) + scale_x_discrete(name = "Elevation") +
   scale_y_continuous(name = "Biomass/day (g)") + ggtitle("Forbs") + scale_fill_brewer(palette = "Accent") + theme_bw() +
   theme(plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
-        text = element_text(size = 12, family = "Tahoma"),
+        text = element_text(size = 10, family = "Tahoma"),
         axis.title = element_text(face="bold"),
         axis.text.x=element_text(size = 11),
         legend.position = "bottom") +
@@ -46,15 +56,17 @@ pF <- ggplot(F, aes(x = Site, y = Fperday, fill = Treatment)) +
   labs(fill = "Treatment")
 pF
 
+##Bryophytes##
+
 pB <- ggplot(F, aes(x = Site, y = Bperday, fill = Treatment)) +
   geom_boxplot()
 pB
 
 pB <- ggplot(F, aes(x = Site, y = Bperday, fill = Treatment)) +
-  geom_boxplot(notch = FALSE) + scale_x_discrete(name = "Study sites") +
+  geom_boxplot(notch = FALSE) + scale_x_discrete(name = "Elevation") +
   scale_y_continuous(name = "Biomass/day (g)") + ggtitle("Bryophytes") + scale_fill_brewer(palette = "Accent") + theme_bw() +
   theme(plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
-        text = element_text(size = 12, family = "Tahoma"),
+        text = element_text(size = 10, family = "Tahoma"),
         axis.title = element_text(face="bold"),
         axis.text.x=element_text(size = 11),
         legend.position = "bottom") +
@@ -62,15 +74,17 @@ pB <- ggplot(F, aes(x = Site, y = Bperday, fill = Treatment)) +
   labs(fill = "Treatment")
 pB
 
+##Lichen##
+
 pL <- ggplot(F, aes(x = Site, y = Lperday, fill = Treatment)) +
   geom_boxplot()
 pL
 
 pL <- ggplot(F, aes(x = Site, y = Lperday, fill = Treatment)) +
-  geom_boxplot(notch = FALSE) + scale_x_discrete(name = "Study sites") +
+  geom_boxplot(notch = FALSE) + scale_x_discrete(name = "Elevation") +
   scale_y_continuous(name = "Biomass/day (g)") + ggtitle("Lichen") + scale_fill_brewer(palette = "Accent") + theme_bw() +
   theme(plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
-        text = element_text(size = 12, family = "Tahoma"),
+        text = element_text(size = 10, family = "Tahoma"),
         axis.title = element_text(face="bold"),
         axis.text.x=element_text(size = 11),
         legend.position = "bottom") +
@@ -78,19 +92,18 @@ pL <- ggplot(F, aes(x = Site, y = Lperday, fill = Treatment)) +
   labs(fill = "Treatment")
 pL
 
-data(F)
-F$Site <- factor(F$Site,
-                 labels = c("V", "H", "J", "B", "L"))
+
+###Shrub##
 
 pS <- ggplot(F, aes(x = Site, y = Sperday, fill = Treatment)) +
   geom_boxplot()
 pS
 
 pS <- ggplot(F, aes(x = Site, y = Sperday, fill = Treatment)) +
-  geom_boxplot(notch = FALSE) + scale_x_discrete(name = "Study sites") +
+  geom_boxplot(notch = FALSE) + scale_x_discrete(name = "Elevation") +
   scale_y_continuous(name = "Biomass/day (g)") + ggtitle("Shrubs") + scale_fill_brewer(palette = "Accent") + theme_bw() +
   theme(plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
-        text = element_text(size = 12, family = "Tahoma"),
+        text = element_text(size = 10, family = "Tahoma"),
         axis.title = element_text(face="bold"),
         axis.text.x=element_text(size = 11),
         legend.position = "bottom") +
@@ -98,9 +111,24 @@ pS <- ggplot(F, aes(x = Site, y = Sperday, fill = Treatment)) +
   labs(fill = "Treatment")
 pS
 
-######################################################################
-####Attempt to put multiple plots together############################
-######################################################################
+#######################################################################
+##Litter per day#######################################################
+
+pLitter <- ggplot(F, aes(x = Site, y = LitterStand, fill = Treatment)) +
+  geom_boxplot(notch = FALSE) + scale_x_discrete(name = "Elevation") +
+  scale_y_continuous(name = "Biomass/day (g)") + ggtitle("Litter") + scale_fill_brewer(palette = "Accent") + theme_bw() +
+  theme(plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
+        text = element_text(size = 10, family = "Tahoma"),
+        axis.title = element_text(face="bold"),
+        axis.text.x=element_text(size = 11),
+        legend.position = "bottom") +
+  scale_fill_brewer(palette = "Accent") +
+  labs(fill = "Treatment")
+pLitter
+
+#######################################################
+####Multiple plots together############################
+#######################################################
 
 if(!require(devtools)) install.packages("devtools")
 devtools::install_github("kassambara/ggpubr")
@@ -108,6 +136,14 @@ devtools::install_github("kassambara/ggpubr")
 install.packages("ggpubr")
 library(ggpubr)
 
-ggarrange(p10, pF, pB, pL, pS + rremove("x.text"), 
-          labels = c("A", "B", "C", "D", "E"),
-          ncol = 3, nrow = 2)
+figure <- ggarrange(pG, pF, pB, pS, pL, pLitter, 
+          labels = c("A", "B", "C", "D", "E", "F"),
+          ncol = 2, nrow = 3, common.legend = TRUE, 
+          legend="bottom")
+
+figure
+
+annotate_figure(figure,
+                top = text_grob("Biomass/day of functional groups in permanent plots", 
+                                color = "black", face = "bold", size = 14))
+
