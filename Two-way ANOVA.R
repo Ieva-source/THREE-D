74a2c135bbd0a1ad7bca7f6402af6f2e936dc774
Ieva-source <petrauskaite.ieva14@gmail.com>
2020-03-01 13:39:58 +0100

    Two-way ANOVA + posthoc

--- Two-way ANOVA.R
+++ Two-way ANOVA.R
@@ -1,24 +1,24 @@
 ###Testing if treament effect varies among sites in permanent plots######
 ########Two-Way ANOVA####################################################
 
-Testing2<-read.csv("Testing_treatment_effect_among_sites.csv",  
+P<-read.csv("Total_Biomass_Permanent_Plots.csv",  
                   sep=";",                        
                   dec=",",                        
                   header=TRUE,                    
                   stringsAsFactors = FALSE)     
 
-attach(Testing2)
+attach(P)
 
-summary(Testing2)
+summary(P)
 
-str(Testing2)
+str(P)
 
-View(Testing2)
+View(P)
 
-################################
-###Testing for the main effect##
+########################################
+###Testing for the main effect BIO/DAY##
 
-Model_1<-aov(biomass~treatment+site, data=Testing2)
+Model_1<-aov(bioperday~Treatment+elevation, data=P)
 
 summary(Model_1)
 
@@ -35,12 +35,34 @@ shapiro.test(uhat1)
 
 ####Assumption 2: Homogeneity of variance of the groups###
 
-leveneTest(biomass~treatment*site, data=Testing2)
+bartlett.test(bioperday~Treatment)
+
+########################################
+###Testing for the main effect BIO/DAY + 32 added to "In between"##
+
+Model_3<-aov(bioperday32~Treatment+elevation, data=P)
+
+summary(Model_3)
+
+###Cheking if the model assumptions are met###
+
+par(mfrow=c(2,2))
+
+plot(Model_3)
+
+#####Assumption 1: Normal distribution: Shapiro-Wilk test (testing for normality)####
+
+uhat3<-resid(Model_3)
+shapiro.test(uhat3)
+
+####Assumption 2: Homogeneity of variance of the groups###
+
+bartlett.test(bioperday32~Site)
 
 ########################################
 ###Testing for the interaction effect###
 
-Model_2<-aov(biomass~treatment*site, data=Testing2)
+Model_2<-aov(bioperday~Treatment*elevation, data=P)
 
 summary(Model_2)
 
@@ -55,19 +77,50 @@ plot(Model_2)
 uhat2<-resid(Model_2)
 shapiro.test(uhat2)
 
-###Post-hoc test: Since we found a significant interaction effect###
+########################################
+###Testing for the interaction effect +32 days added to the "In between" site###
 
-###Question to AUD: what am I doing wrong here?###
-#I constantly get the error message saying that the "cld" function could not be found##
+Model_4<-aov(bioperday32~Treatment*Sitename, data=P)
+
+summary(Model_4)
+
+##Assumption testig:##
+
+par(mfrow=c(2,2))
+
+plot(Model_4)
+
+#####Shapiro-Wilk test (testing for normality)####
+
+uhat4<-resid(Model_4)
+shapiro.test(uhat4)
+
+####Not sure how to run the barlett test for this one###
+
+bartlett.test(bioperday32~Site)
+
+###Post-hoc test: Since we found a significant interaction effect###
 
 library("lsmeans")
 library("multcompView")
 
-posthoc <-lsmeans(Model_2,
-                  pairwise~treatment*site,
+###Pairwise for Sites##
+
+posthoc <-lsmeans(Model_4,
+                  pairwise~Sitename,
                   adjust = "tukey")
 posthoc
 
 cld(posthoc,
     alpha=0.05,
     Letter=letters)
+
+TukeyHSD(Model_4, "Sitename")
+
+###Pairwise for Sites x Treatment##
+
+posthoc <-lsmeans(Model_4,
+                  pairwise~Treatment*Sitename,
+                  adjust = "tukey")
+posthoc
+
